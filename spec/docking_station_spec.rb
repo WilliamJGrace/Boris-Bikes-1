ebfd744040c79759defd7bf27da89cb2bb309124
WilliamJGrace <williamjgrace@outlook.com>
2020-02-20 16:45:05 +0000

    added a test for docking station full; changed code to pass test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,7 +5,7 @@ describe DockingStation do
 
 	it "checks if bike is working" do
 		bike = Bike.new
-		expect(bike).to be_working 
+		expect(bike).to be_working
 	end
 
 	it {is_expected.to respond_to(:dock).with(1).argument}
@@ -20,8 +20,13 @@ describe DockingStation do
 		expect(subject.bike).to eq bike
 	end
 
-	it "raises an error" do
+	it "raises an error when no bikes" do
 		expect {subject.release_bike}.to raise_error("No bikes available")
+	end
+
+	it "raises an error when full" do
+    subject.dock(Bike.new)
+		expect {subject.dock(Bike.new)}.to raise_error("Docking station full")
 	end
 
 end
