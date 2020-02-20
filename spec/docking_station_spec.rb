de88cd0084e393c3b61ff3081732c96bb14b0868
lilawalker <lilawlker@gmail.com>
2020-02-20 16:32:09 +0000

    Add exception error for no bikes avaialable

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,7 +2,12 @@ require 'docking_station.rb'
 
 describe DockingStation do
 	it {is_expected.to respond_to(:release_bike)}
-	it { expect(subject.release_bike).to be_working }
+
+	it "checks if bike is working" do
+		bike = Bike.new
+		expect(bike).to be_working 
+	end
+
 	it {is_expected.to respond_to(:dock).with(1).argument}
 	it {is_expected.to respond_to(:bike)}
 	it 'docks something' do
@@ -15,5 +20,8 @@ describe DockingStation do
 		expect(subject.bike).to eq bike
 	end
 
+	it "raises an error" do
+		expect {subject.release_bike}.to raise_error("No bikes available")
+	end
 
 end
