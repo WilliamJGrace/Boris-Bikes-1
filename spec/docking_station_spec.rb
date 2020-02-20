2903893e975cf2ec7349e2fed2b7d775a1db04e6
WilliamJGrace <williamjgrace@outlook.com>
2020-02-20 15:27:11 +0000

    Check that Docking station responds to both dock and bike; That dock can accept an argument (bike); test that DockinStation can return a docked bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,4 +3,17 @@ require 'docking_station.rb'
 describe DockingStation do
 	it {is_expected.to respond_to(:release_bike)}
 	it { expect(subject.release_bike).to be_working }
+	it {is_expected.to respond_to(:dock).with(1).argument}
+	it {is_expected.to respond_to(:bike)}
+	it 'docks something' do
+		bike = Bike.new
+		expect(subject.dock(bike)).to eq bike
+	end
+	it 'returns docked bikes' do
+		bike = Bike.new
+    subject.dock(bike)
+		expect(subject.bike).to eq bike
+	end
+
+
 end
